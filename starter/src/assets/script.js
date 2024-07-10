dc61263c955e3c360c6e716e43bfb7f9c003e75b
dotstry <dotstry@gmail.com>
2024-07-10 15:16:48 -0700

    converted products from individual variables to array components under the 'products' array

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,44 +1,37 @@
 // Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-products = [cherry, orange, strawberry];
-
-
-// Create 3 or more product objects using object literal notation 
-// Each product should include five properties
-// name: name of product (string)
-// price: price of product (number)
-// quantity: quantity in cart should start at zero (number)
-// productId: unique id for the product (number)
-// image: picture of product (url string)
-
-
-const cherry =
+const products = [
 {
-     name: "Cherry"
-     price: 0.33
-     quantity: 0
-     productID: 1
+     // Create 3 or more product objects using object literal notation 
+     // Each product should include five properties
+     // name: name of product (string)
+     name: "Cherry",
+     // price: price of product (number)
+     price: 0.33,
+     // quantity: quantity in cart should start at zero (number)
+     quantity: 0,
+     // productId: unique id for the product (number)
+     productID: 0,
+     // image: picture of product (url string)
+     // Images provided in /images folder. All images from Unsplash.com
      // cherry.jpg by Mae Mu
-     image: "../images/cherry.jpg"
-}
-const orange = 
+     image: "../src/images/cherry.jpg",
+},
 {
-     name: "Orange"
-     price: 1.10
-     quantity: 
-     productID: 2
-     // orange.jpg by Mae Mu
-     image: "../images/orange.jpg"
-}
-const strawberry = 
+     name: "Orange",
+     price: 1.11,
+     quantity:0 ,
+     productID: 1,
+     // orange.jpg by Mae Mu;
+     image: "../src/images/orange.jpg",
+},
 {
-     name: "Strawberry"
-     price: 0.44
-     quantity: 0
-     productID: 3
+     name: "Strawberry",
+     price: 0.44,
+     quantity: 0,
+     productID: 2,
      // strawberry.jpg by Allec Gomes
-     image: "../images/strawberry.jpg"
-}
-// Images provided in /images folder. All images from Unsplash.com
+     image: "../src/images/strawberry.jpg",
+}]
 
 
 /* Declare an empty array named cart to hold the items in the cart */
@@ -49,12 +42,34 @@ const cart = [];
 function addProductToCart(productID)
 {
 //- addProductToCart should get the correct product based on the productId
-
+//- if the product is not already in the cart, add it to the cart
+let correctProduct = 0;
+// one of these is correct
+products.forEach (product in products)
+{
+     if (product.productID === productID);
+}
+// or
+// for (n=0; n<products.length; n++)
+/* switch(productID)
+{
+     case 0:
+          product0.quantity++;
+          break;
+     case 1:
+          product1.quantity++;
+          break;
+     case 2:
+          product2.quantity++;
+          break;
+     default:
+          break; 
+}*/
 //- addProductToCart should then increase the product's quantity
 
-//- if the product is not already in the cart, add it to the cart
-}
 
+
+}
 
 // Create a function named increaseQuantity that takes in the productId as an argument
 function increaseQuantity(productID)
@@ -75,6 +90,7 @@ function decreaseQuantity(productID)
 // decreaseQuantity should decrease the quantity of the product
 
 // if the function decreases the quantity to 0, the product is removed from the cart
+
 }
 
 
@@ -96,14 +112,18 @@ function cartTotal()
 //- cartTotal should iterate through the cart to get the total cost of all products
 //- cartTotal should return the total cost of the products in the cart
 //Hint: price and quantity can be used to determine total cost
+     let totalConst = 0.00;
+     cart.forEach()
+     {
 
+     }
 }
 
 
 /* Create a function called emptyCart that empties the products from the cart */
 function emptyCart()
 {
-
+     cart = [];
 }
 
 // Create a function named pay that takes in an amount as an argument
